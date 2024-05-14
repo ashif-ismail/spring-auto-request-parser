50acccb80b4a01cb3f7a8ed4250187478b4e6de9
Ashif Ismail <cyber2maro@gmail.com>
2024-05-14 17:10:38 +0400

    fixed auto-configure pickup issue

--- src/main/java/com/ashifismail/autorequest/config/WebAutoConfig.java
+++ src/main/java/com/ashifismail/autorequest/config/WebAutoConfig.java
@@ -5,8 +5,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,14 +19,16 @@ import java.util.List;
  * @author Ashif Ismail
  * @email ashifismail.ae@gmail.com
  */
+
 @Configuration
+@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
 public class WebAutoConfig implements WebMvcConfigurer {
 
     private static final Logger logger = LoggerFactory.getLogger(WebAutoConfig.class);
 
     @Bean
     public RequestContentArgumentResolver requestContentArgumentResolver() {
-        logger.info("RequestContentArgumentResolver bean created");
+        logger.info("Auto Request resolver bean created");
         return new RequestContentArgumentResolver(objectMapper());
     }
 
@@ -36,6 +41,6 @@ public class WebAutoConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(requestContentArgumentResolver());
-        logger.info("RequestContentArgumentResolver added to resolvers");
+        logger.info("Auto Request resolver added to resolvers");
     }
 }
